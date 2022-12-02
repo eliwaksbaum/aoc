b5ff8eb30541f7d1c7590de274e7cdc9e0cb10be
Eli Waksbaum <eli@waksbaum.com>
2022-12-02 13:44:58 -0500

    Day 1

---
+++ 1/Program.cs
@@ -0,0 +1,15 @@
+﻿string input = File.ReadAllText("input.txt");
+string[] elves = input.Split("\n\n");
+
+List<int> calories = elves.Select((elf, i) => elf.Split("\n"))
+    .Select((snacks, i) => 
+        snacks.Select((snack, j) => Int32.Parse(snack))
+            .Sum()
+    ).ToList();
+    
+//1
+Console.WriteLine(calories.Max());
+
+//2
+int top3 = calories.Order().Reverse().Take(3).Sum();
+Console.WriteLine(top3);
