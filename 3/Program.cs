d19a3e1291140fd3825746cb6e86cd9d23d48fae
Eli Waksbaum <eli@waksbaum.com>
2022-12-03 13:03:48 -0500

    Day 3

---
+++ 3/Program.cs
@@ -0,0 +1,21 @@
+﻿string[] rucksacks = File.ReadAllLines("input.txt");
+
+//1
+int item_sum = rucksacks.Select((r, i) => r.Chunk(r.Length/2).ToList())
+    .Select((compartments, i) => compartments[0].Intersect(compartments[1]))
+    .Select((shared, i) => (int) shared.First())
+    .Select((ascii, i) => ascii > 96 ? ascii - 96 : ascii - 38)
+    .Sum();
+
+Console.WriteLine(item_sum);
+
+//2
+int badge_sum = rucksacks.Chunk(3)
+    .Select((group, i) =>
+        group.Aggregate((a, b) => new string(a.Intersect(b).ToArray()))
+    )
+    .Select((shared, i) => (int) shared.First())
+    .Select((ascii, i) => ascii > 96 ? ascii - 96 : ascii - 38)
+    .Sum();
+
+Console.WriteLine(badge_sum);
