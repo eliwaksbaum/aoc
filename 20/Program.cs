2349bd1bdfec706dc3c67b2b3ac4b721fc2c12ca
Eli Waksbaum <eli@waksbaum.com>
2023-12-01 09:24:49 -0500

    unfinished days

---
+++ 20/Program.cs
@@ -0,0 +1,10 @@
+﻿List<int> foo = File.ReadAllLines("input.txt").Select(x => int.Parse(x)).ToList();
+
+Console.WriteLine(foo.Count * foo.Count);
+
+int L = foo.Count;
+int asks = foo.Select(x => ((Math.Abs(x)/L + 1) * L) + x)
+    .Select(x => x%L)
+    .Sum();
+
+Console.WriteLine(asks);
