707e7180d40941a8b3a17c9883f6c6955fb72ef0
Eli Waksbaum <eli@waksbaum.com>
2023-12-20 15:56:36 -0500

    stash

---
+++ 2023/Day5.cs
@@ -0,0 +1,10 @@
+namespace AoC
+{
+    public class Day5
+    {
+        public static void Go()
+        {
+
+        }
+    }
+}
--- 2023/src/main.rs
+++
@@ -1,8 +0,0 @@
-use std::fs;
-
-mod day_1;
-
-fn main()
-{
-    day_1::go(fs::read_to_string("1.txt").unwrap())
-}
